e94d60d866992791812290db7551b963f190a5db
Mark Armendariz <src@enobrev.com>
2018-03-27 00:40:41 -0500

    Added script for downloading flattened config for packer

---
+++ src/tools/download_packer_config.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Downloads everything in the parameter store under the environment path, but with keys flattened rather than as a
+
+const ParameterStore = require('aws-parameter-store').default;
+const fs             = require('fs');
+const {
+    flattenObject,
+    sortObject
+}                    = require('./tools');
+
+const ENVIRONMENT    = process.env['ENVIRONMENT'];
+const AWS_REGION     = process.env['AWS_REGION'];
+
+if (!ENVIRONMENT) {
+    throw new Error('ENVIRONMENT needs to be set')
+}
+
+if (!AWS_REGION) {
+    throw new Error('AWS_REGION needs to be set')
+}
+
+const PATH = (process.argv.length > 2 && process.argv.slice(2).shift().replace(/[/+]$/, '')) || '.';
+
+ParameterStore.setRegion(AWS_REGION);
+ParameterStore.mergePathsAsObject([
+    `/${ENVIRONMENT}`
+], (oError, oConfig) => {
+    if (oError) {
+        console.error('ERROR', oError);
+        process.exit(1);
+    }
+
+    const oFlattened = flattenObject(oConfig, '.');
+    const oSorted    = sortObject(oFlattened);
+    const sStringed  = JSON.stringify(oSorted, null, '    ');
+
+    fs.writeFileSync(`${PATH}/config.packer.json`, sStringed);
+});
